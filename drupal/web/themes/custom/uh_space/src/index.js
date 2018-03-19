952133708e8cbc9880bf99b0b61a590703f95b03
Otso Lahti <otso.lahti@wunder.io>
2018-03-19 16:04:04 +0200

    HT-20: Implemented multiselect using select2 library.

--- drupal/web/themes/custom/uh_space/src/index.js
+++ drupal/web/themes/custom/uh_space/src/index.js
@@ -1,2 +1,13 @@
 // Require application style
-require('./main.scss')
+require('./main.scss');
+
+(function ($, Drupal) {
+  Drupal.behaviors.uhMultiselect = {
+    attach: function (context, settings) {
+      // Multiselect with Select2.
+      $('select[multiple="multiple"]').select2({
+        theme: 'default'
+      });
+    }
+  };
+})(jQuery, Drupal);
