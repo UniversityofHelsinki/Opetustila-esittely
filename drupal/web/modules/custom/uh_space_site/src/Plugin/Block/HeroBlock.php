681aab891e6eac35ec6d79851b265bbe74890888
Otso Lahti <otso.lahti@wunder.io>
2018-03-20 13:36:35 +0200

    HT-71: Front page content type (page). Custom hero block. Unique hero styling.

---
+++ drupal/web/modules/custom/uh_space_site/src/Plugin/Block/HeroBlock.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Drupal\uh_space_site\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a Hero Block.
+ *
+ * @Block(
+ *   id = "hero_block",
+ *   admin_label = @Translation("Hero Block"),
+ *   category = @Translation("UH Space Site"),
+ * )
+ */
+class HeroBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $node = \Drupal::routeMatch()->getParameter('node');
+
+    $block = array(
+      '#theme' => 'hero_block',
+      '#title' => $node->getTitle(),
+    );
+
+    $lead = $node->field_lead_text->getValue();
+    if (!empty($lead[0]['value'])) {
+      $block['#lead'] = $lead[0]['value'];
+    }
+
+    $image = $node->field_featured_image->getValue();
+    if (!empty($image[0]['target_id'])) {
+      $file = \Drupal\file\Entity\File::load($image[0]['target_id']);
+      $path = $file->getFileUri();
+      $url = \Drupal\image\Entity\ImageStyle::load('hero_image')->buildUrl($file->getFileUri());
+      $block['#background_image'] = $url;
+    }
+
+    return $block;
+  }
+
+}
