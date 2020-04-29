1cfeda636c1d60b37cb0e8e82245fd54aa64e362
Valters Š <mr.valters@gmail.com>
2020-04-29 13:50:21 +0400

    HT-125 | Leaflet map controlls (title & aria-label) translated.

---
+++ drupal/web/modules/custom/uh_space_site/assets/js/leaflet_map.js
@@ -0,0 +1,33 @@
+(function ($, Drupal) {
+  Drupal.behaviors.leaflet_map = {
+    attach(context, settings) {
+
+      // Get language code
+      const {currentLanguage} = settings['path'];
+
+      // Prepare title texts depending on language
+      let titles = {};
+      switch (currentLanguage) {
+        case 'fi':
+          titles.zoomInTitle = 'Lähennä';
+          titles.zoomOutTitle = 'Pienennä';
+          break;
+        // Assume default is English
+        default:
+          titles.zoomInTitle = 'Zoom In';
+          titles.zoomOutTitle = 'Zoom Out';
+      }
+
+      // Define dom elements
+      const zoomInElement = $('.leaflet-control-zoom-in');
+      const zoomOutElement = $('.leaflet-control-zoom-out');
+
+      // Set titles to the elements
+      zoomInElement.attr('aria-label', titles.zoomInTitle);
+      zoomInElement.prop('title', titles.zoomInTitle);
+
+      zoomOutElement.attr('aria-label', titles.zoomOutTitle);
+      zoomOutElement.prop('title', titles.zoomOutTitle);
+    },
+  };
+}(jQuery, Drupal));
