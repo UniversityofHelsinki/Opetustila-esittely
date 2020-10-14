2670bdc28bc208b1ed1f4b9257a911d40c741d58
Tormi Tabor <tormi.tabor@gmail.com>
2020-10-14 14:44:39 +0300

    HT-135: Add Warden 2.0 setup.

--- drupal/conf/lando/update.sh
+++ drupal/conf/lando/update.sh
@@ -7,6 +7,9 @@ drush @tilat.local updb -y
 # Enable development modules.
 drush @tilat.local en update stage_file_proxy -y
 
+# Disable production modules.
+drush @tilat.local pmu warden -y
+
 # Clear caches.
 drush @tilat.local cc drush
 drush @tilat.local cr -y
