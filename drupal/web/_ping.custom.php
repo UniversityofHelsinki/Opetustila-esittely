bb5f03bc3101bbe7d51b31462aa277a4aef2e2eb
Deivids Briedis <deivids.briedis@wunder.io>
2023-07-13 13:37:45 +0300

    ht router, custom ping and robots txt

---
+++ drupal/web/_ping.custom.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @file
+ * This is custom ping code.
+ *
+ * Note: tests can mess up this file - disregard the changes.
+ */
+
+$status = 'warning';
+$message = 'Too bad.';
+$payload = [
+  'file' => '/tmp/xxx',
+  'count' => 99,
+];
+
+sleep(1);
