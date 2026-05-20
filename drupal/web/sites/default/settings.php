1099d1f4fe44247a155abe0cd97d8e2eeb5e7561
Gervin Kalvet <gervin.kalvet@wunder.io>
2026-05-20 13:28:09 +0300

    Core update w/ modules
    Prep for later big security update.
    10.1.8 => 10.6.8

--- drupal/web/sites/default/settings.php
+++ drupal/web/sites/default/settings.php
@@ -211,3 +211,9 @@ if (file_exists(__DIR__ . '/settings.local.php')) {
 }
 
 $settings['config_exclude_modules'] = ['devel', 'stage_file_proxy'];
+
+// Automatically generated include for settings managed by ddev.
+$ddev_settings = __DIR__ . '/settings.ddev.php';
+if (getenv('IS_DDEV_PROJECT') == 'true' && is_readable($ddev_settings)) {
+  require $ddev_settings;
+}
